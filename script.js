433835404d05d698174cfda31b3188cf52151f76
Egor <cefranovegor@gmail.com>
2024-10-15 22:56:02 +0300

    Добавлевны изменения

---
+++ script.js
@@ -0,0 +1,149 @@
+// let num = 10
+// if (num) {
+//  console.log ('число' + num + 'положительное');//первый способ
+//  console.log (`число ${num} положительное`);//второй способ
+// } else if (num<0){
+//     console.log (`число ${num} отрицательное`)
+// }else {
+//     console.log (`число ${num}`)
+// }
+
+// if (num>0) {
+
+// }else if (num<0){
+// }else if (num>0){ 
+// }else{
+
+// }
+
+// let num2 = -7 
+// if (num2<0) {
+//     console.log(-num2);
+// }else {
+//     console.log(num2);
+// }
+
+// let month
+// switch (key) {
+//     case 1:
+//           console.log('Январь');
+//         break;
+//         case 1:
+//             console.log('Февраль');
+//           break;
+//           case 2:
+//             console.log('Март');
+//           break;
+//           case 3:
+//             console.log('Апрель');
+//           break;
+//           case 4:
+//             console.log('Май');
+//           break;
+//           case 5:
+//             console.log('Июнь');
+//           break;
+//           case 6:
+//             console.log('Июль');
+//           break;  
+//           case 7:
+//             console.log('Август');
+//           break;     
+//           case 8:
+//             console.log('Сентябрь');
+//           break;     
+//           case 9:
+//             console.log('Октябрь');
+//           break;  
+//           case 10:
+//             console.log('Ноябрь');
+//           break; 
+//           case 11:
+//             console.log('Декабрь');
+//           break;
+//           case 12:
+//             console.log('Август');
+//           break;                                                                                                          
+//         default:
+//             console.logv('значение не определено')
+//             break;
+// }
+// let number1 = +prompt('введите нужный символ')
+// let number2 = 10
+// let operator = '+'
+
+// switch (operator) {
+//   case'+':
+//     console.log(number1 + number2);
+//     break;
+//   case'-':
+//     console.log(number1 - number2);
+//     break;
+//   case'*':
+//     console.log(number1 * number2);
+//     break;
+//  case'/':
+//     console.log(number1 / number2);
+//     break;
+
+//     default:
+//         console.log('Значение не определено')
+//     break;
+
+//     let num1 = 6
+//     let num2 = 5
+//     //тернарный оператор (условие) ,(если условное истино, то сработает первый вариант) : (иначе,если )
+//     num1>num2 ? console.log(`число ${num1} больше чем ${num2}`): console.log(`число ${num2} больше чем ${num1}`);
+
+//     let y = 20
+//     //первый способ
+//     i (confition) 
+//   if (y%5==0){
+//     console.log(`число ${y} кратно 5`)
+//     } else {
+//         console.log(`число ${y} кратно 5`)
+//     }
+
+   
+//     y%5==0   //второй способ
+//  ? console.log(`число ${y} кратно 5`) // если условие истино, то сработает первый вариант
+// :console.log(`число ${y} кратно 5`) //иначе, если ложно, то сработает второй вариант
+
+// let age = 2024
+
+// if (age%4==0) { // 400 800 1200 1600 2000
+//      console.log('год високосный');
+// }else if (age%100!=0){
+//     console.log('год невисокосный');
+// }else if (age%4!=0){
+//     console.log('год високосный');
+// }else {
+//     console.log('год невисокосный');
+// }
+
+// let number ='323'
+// console.log(number);
+// let checkNumber = number.split('')
+// for (let i = 0; i <checkNumber.length; i++){
+//     if(checkNum[1] == checkNum[2]){
+//         console.log('числа равнозначны');
+//     }else{
+//        console.log('числа не равнозначны ')
+//     }
+    
+// } 
+// console.log(checkNumber);
+
+
+
+
+                                                                                            
+
+
+
+
+
+
+
+
+
